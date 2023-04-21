79eeb8fd2ed4c7e4e8feaacd4f945ed8a6f37582
sajad mehri <sajadmhr666@gmail.com>
2023-04-21 20:12:51 +0330

    upload files

---
+++ js/script.js
@@ -0,0 +1,50 @@
+let is_open = false;
+function openNavbarMobile() {
+  console.log(is_open);
+  if (!is_open) {
+    $("#menu-icon").removeClass("bi-list");
+    $("#menu-icon").addClass("bi-plus-lg");
+    $("#menu-icon").addClass("close-menu-btn");
+    $(".mobile-navigation-container").addClass("open");
+    $("body").css("overflow-y", "hidden");
+    is_open = true;
+  } else {
+    $("#menu-icon").removeClass("bi-plus-lg");
+    $("#menu-icon").addClass("bi-list");
+    $("#menu-icon").removeClass("close-menu-btn");
+    $(".mobile-navigation-container").removeClass("open");
+    $("body").css("overflow-y", "scroll");
+    is_open = false;
+  }
+}
+
+$(".open-menu-btn").on("click", () => {
+  openNavbarMobile();
+});
+
+$("#showMoreButton").on("click", () => {
+  $(".future-container").css("max-height", "9999px");
+  $(".show-more-future").css("display", "none");
+});
+
+let is_open_store = false;
+function openInstallAppNavBar() {
+  if (!is_open_store) {
+    $("#install-app-icon").removeClass("bi-chevron-down");
+    $("#install-app-icon").addClass("bi-chevron-up");
+    $(".install-app-store").removeClass("close-install")
+    $(".install-app-store").addClass("open-install")
+    is_open_store = true;
+  } else {
+    $("#install-app-icon").removeClass("bi-chevron-up");
+    $("#install-app-icon").addClass("bi-chevron-down");
+    $(".install-app-store").removeClass("open-install")
+    $(".install-app-store").addClass("close-install")
+    is_open_store = false;
+  }
+}
+
+$(".open-install-app-navigation").on("click", () => {
+  openInstallAppNavBar();
+});
+$(".install-app-store");
